7ae942a1d01bb3828ffbe21add8aa64453bec646
Jared <133932172+JaredMCarrington@users.noreply.github.com>
2025-04-03 20:03:29 -0400

    completed lab 9

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,11 +1,13 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBnyTSJNB9SOsx1-RPySp7OOzEOK06rM0c",
-    authDomain: "info1601-booksapp.firebaseapp.com",
-    projectId: "info1601-booksapp",
-    storageBucket: "info1601-booksapp.firebasestorage.app",
-    messagingSenderId: "568459881258",
-    appId: "1:568459881258:web:a37d518df9686e77120c53"
-  };
+  apiKey: "AIzaSyCznBgi5yWouwONnbCyBlh2svWhmD__Bn8",
+  authDomain: "lab-9-60f48.firebaseapp.com",
+  projectId: "lab-9-60f48",
+  storageBucket: "lab-9-60f48.firebasestorage.app",
+  messagingSenderId: "252390710164",
+  appId: "1:252390710164:web:9a164f6302d08019994d81",
+  measurementId: "G-FWFQ2XSZ9L"
+};
+
 
 
   export default firebaseConfig;
